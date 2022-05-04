d0d2da54a77cb9b5af84c5e328857cc97ece3444
Zhaulanbekk <zhaulanbek00>
2022-05-04 16:25:06 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Triangle triangle=new Triangle();
+        triangle.a=3;
+        triangle.b=3;
+        triangle.c=3;
+        triangle.area();
+    }
+}
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Triangle {
+    int a;
+    int b;
+    int c;
+    void area(){
+        System.out.println(a*b*c);
+    }
+}
